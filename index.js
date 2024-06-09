57f93c4f4c8ad273767a0a64261b968030dba64e
muhumbania <franklinmuhumbania@gmail.com>
2024-06-09 23:45:42 +0300

    Background made dynamic

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const next = document.querySelector('.n-arrow');
 const prev = document.querySelector('.p-arrow');
 
 let counter = 0;
-let counter2 = 7;
 
 next.addEventListener('click', function(){
     counter+= 1;
@@ -14,7 +13,7 @@ next.addEventListener('click', function(){
     });
 
     console.log(counter);
-    document.getElementById('8').setAttribute('src', `${document.getElementById('7').getAttribute('src')}`)
+    document.querySelector('.container').style.backgroundImage = `url(${document.getElementById('7').getAttribute('src')})`;
 });
 
 
@@ -24,7 +23,7 @@ prev.addEventListener('click', function(){
         const img = item.children;
         img[0].setAttribute('src', `./${(item.id-counter)<1? (item.id-counter)+6 : item.id-counter}.PNG`);
     });
-
+    document.querySelector('.container').style.backgroundImage = `url(${document.getElementById('7').getAttribute('src')})`;
     if(counter<1)counter = 6;
 });
 
